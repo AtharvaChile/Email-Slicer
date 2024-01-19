61048a4acda8a12cfd8498f89e3963ec5c66a477
Atharva Chile <145362478+AtharvaChile@users.noreply.github.com>
2024-01-19 20:02:17 +0530

    Add files via upload

---
+++ email slicer.py
@@ -0,0 +1,14 @@
+#Getting user email address
+email = input("What is your email address?:").strip()
+
+#Slice user name
+user = email[:email.index("@")]
+
+#slice domain name
+domain = email[email.index("@")+ 1:]
+
+#format message
+output = "Your username is {} and your domain name is {}".format(user,domain)
+
+#display output message
+print(output)
